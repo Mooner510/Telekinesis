774e2c2175e51fe71e49537f56d1134cd8c8f1fc
Mooner510 <lmck510@gmail.com>
2023-07-18 12:54:31 +0900

    Ores only

--- src/main/java/org/mooner510/telekinesis/Telekinesis.java
+++ src/main/java/org/mooner510/telekinesis/Telekinesis.java
@@ -83,29 +83,29 @@ public final class Telekinesis extends JavaPlugin implements Listener {
             case COAL_ORE -> itemStack.setType(Material.COAL);
             case EMERALD_ORE -> itemStack.setType(Material.EMERALD);
             case NETHER_QUARTZ_ORE -> itemStack.setType(Material.QUARTZ);
-            case SAND, RED_SAND -> itemStack.setType(Material.GLASS);
-            case COBBLESTONE -> itemStack.setType(Material.SMOOTH_STONE);
-            case SANDSTONE -> itemStack.setType(Material.SMOOTH_SANDSTONE);
-            case RED_SANDSTONE -> itemStack.setType(Material.SMOOTH_RED_SANDSTONE);
-            case CLAY_BALL -> itemStack.setType(Material.BRICK);
-            case NETHERRACK -> itemStack.setType(Material.NETHER_BRICK);
-            case NETHER_BRICKS -> itemStack.setType(Material.CRACKED_NETHER_BRICKS);
-            case BASALT -> itemStack.setType(Material.SMOOTH_BASALT);
-            case CLAY -> itemStack.setType(Material.TERRACOTTA);
-            case STONE_BRICKS -> itemStack.setType(Material.CRACKED_STONE_BRICKS);
-            case POLISHED_BLACKSTONE_BRICKS -> itemStack.setType(Material.CRACKED_POLISHED_BLACKSTONE_BRICKS);
-            case COBBLED_DEEPSLATE -> itemStack.setType(Material.DEEPSLATE);
-            case DEEPSLATE_BRICKS -> itemStack.setType(Material.CRACKED_DEEPSLATE_BRICKS);
-            case DEEPSLATE_TILES -> itemStack.setType(Material.CRACKED_DEEPSLATE_TILES);
-            case CACTUS -> itemStack.setType(Material.GREEN_DYE);
-            case ACACIA_LOG, BIRCH_LOG, OAK_LOG, DARK_OAK_LOG, JUNGLE_LOG, SPRUCE_LOG, MANGROVE_LOG,
-                    STRIPPED_ACACIA_LOG, STRIPPED_OAK_LOG, STRIPPED_JUNGLE_LOG, STRIPPED_BIRCH_LOG, STRIPPED_DARK_OAK_LOG, STRIPPED_SPRUCE_LOG, STRIPPED_MANGROVE_LOG,
-                    ACACIA_WOOD, BIRCH_WOOD, DARK_OAK_WOOD, JUNGLE_WOOD, MANGROVE_WOOD, OAK_WOOD, SPRUCE_WOOD,
-                    STRIPPED_ACACIA_WOOD, STRIPPED_DARK_OAK_WOOD, STRIPPED_BIRCH_WOOD, STRIPPED_JUNGLE_WOOD, STRIPPED_OAK_WOOD, STRIPPED_MANGROVE_WOOD, STRIPPED_SPRUCE_WOOD ->
-                    itemStack.setType(Material.CHARCOAL);
-            case CHORUS_FRUIT -> itemStack.setType(Material.POPPED_CHORUS_FRUIT);
-            case WET_SPONGE -> itemStack.setType(Material.SPONGE);
-            case SEA_PICKLE -> itemStack.setType(Material.LIME_DYE);
+//            case SAND, RED_SAND -> itemStack.setType(Material.GLASS);
+//            case COBBLESTONE -> itemStack.setType(Material.SMOOTH_STONE);
+//            case SANDSTONE -> itemStack.setType(Material.SMOOTH_SANDSTONE);
+//            case RED_SANDSTONE -> itemStack.setType(Material.SMOOTH_RED_SANDSTONE);
+//            case CLAY_BALL -> itemStack.setType(Material.BRICK);
+//            case NETHERRACK -> itemStack.setType(Material.NETHER_BRICK);
+//            case NETHER_BRICKS -> itemStack.setType(Material.CRACKED_NETHER_BRICKS);
+//            case BASALT -> itemStack.setType(Material.SMOOTH_BASALT);
+//            case CLAY -> itemStack.setType(Material.TERRACOTTA);
+//            case STONE_BRICKS -> itemStack.setType(Material.CRACKED_STONE_BRICKS);
+//            case POLISHED_BLACKSTONE_BRICKS -> itemStack.setType(Material.CRACKED_POLISHED_BLACKSTONE_BRICKS);
+//            case COBBLED_DEEPSLATE -> itemStack.setType(Material.DEEPSLATE);
+//            case DEEPSLATE_BRICKS -> itemStack.setType(Material.CRACKED_DEEPSLATE_BRICKS);
+//            case DEEPSLATE_TILES -> itemStack.setType(Material.CRACKED_DEEPSLATE_TILES);
+//            case CACTUS -> itemStack.setType(Material.GREEN_DYE);
+//            case ACACIA_LOG, BIRCH_LOG, OAK_LOG, DARK_OAK_LOG, JUNGLE_LOG, SPRUCE_LOG, MANGROVE_LOG,
+//                    STRIPPED_ACACIA_LOG, STRIPPED_OAK_LOG, STRIPPED_JUNGLE_LOG, STRIPPED_BIRCH_LOG, STRIPPED_DARK_OAK_LOG, STRIPPED_SPRUCE_LOG, STRIPPED_MANGROVE_LOG,
+//                    ACACIA_WOOD, BIRCH_WOOD, DARK_OAK_WOOD, JUNGLE_WOOD, MANGROVE_WOOD, OAK_WOOD, SPRUCE_WOOD,
+//                    STRIPPED_ACACIA_WOOD, STRIPPED_DARK_OAK_WOOD, STRIPPED_BIRCH_WOOD, STRIPPED_JUNGLE_WOOD, STRIPPED_OAK_WOOD, STRIPPED_MANGROVE_WOOD, STRIPPED_SPRUCE_WOOD ->
+//                    itemStack.setType(Material.CHARCOAL);
+//            case CHORUS_FRUIT -> itemStack.setType(Material.POPPED_CHORUS_FRUIT);
+//            case WET_SPONGE -> itemStack.setType(Material.SPONGE);
+//            case SEA_PICKLE -> itemStack.setType(Material.LIME_DYE);
         }
     }
 
